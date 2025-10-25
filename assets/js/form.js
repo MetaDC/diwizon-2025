79a5cf5cb9579f9d7836d8389d8a3c6d511d102d
MetaDC <150433207+MetaDC@users.noreply.github.com>
2025-10-25 18:07:35 +0530

    .

--- assets/js/form.js
+++ assets/js/form.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function generateCaptchaCode() {
     const chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789";
     let code = "";
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 4; i++) {
       code += chars[Math.floor(Math.random() * chars.length)];
     }
     return code;
